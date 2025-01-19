5700eea03a1a95ce101cf455e193a163f0ea1a88
Amias Iastus <amiataas@gmail.com>
2025-01-19 15:30:56 +0330

    add: implement RuntimePreemption enum and utility class

---
+++ llvm/RuntimePreemption.hx
@@ -0,0 +1,17 @@
+package llvm;
+
+enum RuntimePreemption {
+	None;
+	Local;
+	Preemptible;
+}
+
+class RuntimePreemptionUtil {
+	public inline function toString(v:RuntimePreemption) {
+		switch v {
+			case None: return "";
+			case Local: return "dso_local";
+			case Preemptible: return "dso_preemptible";
+		}
+	}
+}
